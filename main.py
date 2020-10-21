53acaeb0b934c6c29aa0fd81434dc5af806ef871
Vlad Stanescu <vladstanescu94@gmail.com>
2020-10-21 16:51:21 +0300

    add global words list WIP

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import os
 import pprint
 
 from xml_parser import parseXMLFiles
-from nlp import generateCountDictionaiesList
+from nlp import generateCountDictionaiesList, getFileWords
+from helpers import generateGlobalWordsList
 
 path_mini = os.path.abspath("./Resources/Reuters_34/Training/")
 path_full = os.path.abspath("./Resources/Reuters_7083/Training/")
@@ -11,6 +12,10 @@ globalList = []
 
 if __name__ == "__main__":
     files = parseXMLFiles(path_mini)
+    for file in files:
+        root = file.getroot()
+        file_words = getFileWords(root)
+        generateGlobalWordsList(globalList, file_words)
     dictionaries_list = generateCountDictionaiesList(files)
         
     for d in range(len(dictionaries_list)):
