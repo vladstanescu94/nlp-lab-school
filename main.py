13b7b6c111239d9fa61128381d74db7879274ffd
Vlad Stanescu <vladstanescu94@gmail.com>
2020-10-22 10:04:07 +0300

    remove duplicate file iterations

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import pprint
 
 from xml_parser import parseXMLFiles
-from nlp import generateCountDictionaiesList, getFileWords
-from helpers import generateGlobalWordsList
+from nlp import  generateTopicsDictionary , getFileWords
+from helpers import generateGlobalWordsList, generateWordCountDictionary
 
 path_mini = os.path.abspath("./Resources/Reuters_34/Training/")
 path_full = os.path.abspath("./Resources/Reuters_7083/Training/")
@@ -11,12 +11,22 @@ path_full = os.path.abspath("./Resources/Reuters_7083/Training/")
 globalList = []
 
 if __name__ == "__main__":
+    dictionaries_list = []
+    topics_list = []
+
     files = parseXMLFiles(path_mini)
+
     for file in files:
         root = file.getroot()
         file_words = getFileWords(root)
+
+        file_dict = generateWordCountDictionary(file_words)
+        file_dict['FILE_ID'] = root.attrib['itemid']
+        dictionaries_list.append(file_dict)
+        file_topics = generateTopicsDictionary(root)
+        topics_list.append(file_topics)
+
         generateGlobalWordsList(globalList, file_words)
-    dictionaries_list = generateCountDictionaiesList(files)
         
     for d in range(len(dictionaries_list)):
         pprint.pprint(dictionaries_list[d])
@@ -24,3 +34,6 @@ if __name__ == "__main__":
         
     print("Number of files " + str(len(dictionaries_list)))
     print("Words in global dictionary " + str(len(globalList)))
+    print('----------------------------')
+    for topic in topics_list:
+        print(topic)
--- nlp.py
+++ nlp.py
@@ -1,58 +1,57 @@
-import re
 import nltk
-import string
 import contractions
 
 from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer 
 from helpers import lowercaseWords, removeSymbolsAndNumbers, generateWordCountDictionary
 
-def generateCountDictionaiesList(files):
-    dictionaries_list = []
-
-    for file in files:
-        root = file.getroot()
-        file_words = getFileWords(root)
-        file_dict = generateWordCountDictionary(file_words)
-        file_dict['FILE_ID'] = root.attrib['itemid']
-        dictionaries_list.append(file_dict)
-
-    return dictionaries_list
+def generateTopicsDictionary(root):
+    file_data = {}
+    file_data['FILE_ID'] = root.attrib['itemid']
+    file_topics = []
+    for codes in root.iter('codes'):
+        if codes.attrib['class'] == "bip:topics:1.0":
+            for child in codes:
+                file_topics.append(child.attrib['code'])
+    file_data['FILE_TOPICS'] = file_topics
+    return file_data
 
 def getFileWords(root):
     file_words = []
-    file_words += getTitleTagContent(root)
-    file_words += getTextTagContent(root)
+    title_content = getElementContentFromFile(root, 'title')
+    text_content = getElementContentFromFile(root, 'p')
+    file_content = title_content + text_content
+    file_words += generateWordList(file_content)
     return file_words
 
+def getElementContentFromFile(root, elementName):
+    content = ""
+    for element in root.iter(elementName):
+       content += element.text
+    return content
 
-def getTitleTagContent(root):
-    title_tag = root.find('title')
-    title_content = generateWordList(title_tag.text)
-    return title_content
+def generateWordList(text):
+    clean_text = removeContractions(text)
+    tokens = tokenizeText(clean_text)
+    lower_words = lowercaseWords(tokens)
+    clean_words = removeSymbolsAndNumbers(lower_words)
+    clean_words = removeStopWords(clean_words)
+    return clean_words
 
-def getTextTagContent(root):
-    text_content = []
-    for text_tag in root.findall('text'):
-        for p_tag in text_tag.findall('p'):
-            text_content += generateWordList(p_tag.text)
-    return text_content
+def removeContractions(s):
+    return contractions.fix(s)
 
-def generateWordList(s):
-    tokens = tokenizeSentence(s)
-    words = lowercaseWords(tokens)
-    words = removeContractions(words)
-    words = removeSymbolsAndNumbers(words)
-    words = removeStopWords(words)
-    return words
-
-def tokenizeSentence(s):
+def tokenizeText(s):
     return nltk.word_tokenize(s)
-
-def removeContractions(words):
-    return [contractions.fix(word) for word in words]
 
 def removeStopWords(words):
     stop_words = stopwords.words('english')
+    stop_words.append('reuters')
+    stop_words.append('Reuters')
     return [word for word in words if not word in stop_words]
+
+def lemmatizeWords(words):
+    lemmatizer = WordNetLemmatizer()
+    return [lemmatizer.lemmatize(word) for word in words]
 
 
