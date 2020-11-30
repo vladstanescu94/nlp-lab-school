45d7f4fe1008f1935f82c5e81c53be495a671b59
Vlad Stanescu <vladstanescu94@gmail.com>
2020-11-30 17:53:55 +0200

    Refactor write to file

--- file_manager.py
+++ file_manager.py
@@ -1,27 +1,17 @@
-import os
-import xml.etree.ElementTree as ET
+def write_data_to_arrf_file(filename, global_list, file_data_list):
+    distinct_topics = set()
 
+    for file_data in file_data_list:
+        for topic in file_data.topics:
+            distinct_topics.add(topic)
 
-def parse_xml_files(path):
-    trees = []
-    try:
-        for filename in os.listdir(path):
-            if not filename.endswith('.XML'):
-                continue
-            fullname = os.path.join(path, filename)
-            tree = ET.parse(fullname)
-            trees.append(tree)
-    except FileNotFoundError as file_not_found:
-        print("Error : " + file_not_found)
-    return trees
-
-
-def write_data_to_arrf_file(filename, global_list, distinct_topics, vector_space_matrix, topics_list):
     file_writter = open(filename, "w")
 
     for word in global_list:
         file_writter.write(f'@attribute {word} NUMERIC\n')
 
+    file_writter.write("\n")
+    file_writter.write(f'@attribute class {distinct_topics}\n')
     file_writter.write("\n")
 
     for topic in distinct_topics:
@@ -31,15 +21,13 @@ def write_data_to_arrf_file(filename, global_list, distinct_topics, vector_space
     file_writter.write("@data")
     file_writter.write("\n")
 
-    for index, entry in enumerate(vector_space_matrix):
-        for i, instance in enumerate(entry):
-            if instance == 0:
+    for file_data in file_data_list:
+        for i, entry in enumerate(file_data.vector_representation):
+            if entry == 0:
                 continue
-            file_writter.write(f'{i}:{instance},')
+            file_writter.write(f'{i}:{entry},')
         file_writter.write(' # ')
-        topics = topics_list[index]['FILE_TOPICS']
-        for topic in topics:
-            file_writter.write(f'{topic}, ')
+        for topic in file_data.topics:
+            file_writter.write(f"{topic},")
         file_writter.write("\n")
-
     file_writter.close()
