24c6265bd6b75c1473cbdf57e2ea142ca4ea4b7b
Vlad Stanescu <vladstanescu94@gmail.com>
2020-11-30 17:52:56 +0200

    Remove os from config constants

--- config_constants.py
+++ config_constants.py
@@ -1,4 +1,2 @@
-import os
-
-PATH_MINI = os.path.abspath("./Resources/Reuters_34/Training/")
-PATH_FULL = os.path.abspath("./Resources/Reuters_7083/Training/")
+PATH_MINI = "./Resources/Reuters_34/Training/"
+PATH_FULL = "./Resources/Reuters_7083/Training/"
