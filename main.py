50afc4fd8ec0a0f3e6b16faaff2b39ad36e72345
Vlad Stanescu <vladstanescu94@gmail.com>
2021-01-18 17:04:20 +0200

    Refactor mutual information selection

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import os
 import bisect
-import random
 import numpy as np
 
 from sklearn.metrics import normalized_mutual_info_score as score
-from sklearn import feature_selection
 from config_constants import PATH_MINI, PATH_FULL
 from file_data import FileData
 from xml_manager import parse_xml_files
@@ -14,7 +12,7 @@ from file_data_manager import FileDataManager
 
 
 def main():
-    files = parse_xml_files(os.path.abspath(PATH_MINI))
+    files = parse_xml_files(os.path.abspath(PATH_FULL))
     no_examples = len(files)
     global_list = []
     file_data_list = []
@@ -43,32 +41,14 @@ def main():
     print(f"Number of files {no_examples} ")
     print(f"Words in global dictionary {len(global_list)}")
 
-    vector_matrix = []
-
-    for file_data in file_data_list:
-        if not file_data.topics:
-            continue
-
-        for _ in file_data.topics:
-            vector_matrix.append(file_data.vector_representation)
+    vector_matrix = generate_vector_matrix(file_data_list)
 
     npmatrix = np.asarray(vector_matrix)
-    test_prag = 0.6
-    bad_index = []
-    for i in range(npmatrix.shape[1]):
-        for j in range(1, npmatrix.shape[1]):
-            if i != j:
-                if i not in bad_index:
-                    if j not in bad_index:
-                        col_1 = npmatrix[:, i]
-                        col_2 = npmatrix[:, j]
-                        col_score = score(col_1, col_2)
-                        if col_score > test_prag:
-                            bad_index.append(i)
-                            bad_index.append(j)
-                            break
 
-    bad_set = set(bad_index)
+    bad_set = generate_bad_attributes_dict(0.58, npmatrix)
+
+    print("Starting feature selection")
+
     new_global = []
     for i in range(len(global_list)):
         if i in bad_set:
@@ -78,16 +58,48 @@ def main():
     for file_data in file_data_list:
         file_data.generate_vector_representation(new_global)
 
+    print("Feature selection done")
+
     print(len(new_global))
-    # print(npmatrix.shape)
-    # random_index1 = random.randint(0, len(npmatrix[0]))
-    # random_index2 = random.randint(0, len(npmatrix[0]))
-    # col_test_1 = npmatrix[:, random_index1]
-    # col_test_2 = npmatrix[:, random_index2]
-    # print(f"{col_test_1}\n{col_test_2}")
-    # print(score(col_test_1, col_test_2))
-    write_data_to_arrf_file("data2.arrf", new_global,
+
+    write_data_to_arrf_file("data-full-final.arrf", new_global,
                             file_data_manager)
+
+
+def generate_vector_matrix(file_data_list):
+    print("Generating vector matrix")
+    vector_matrix = []
+
+    for file_data in file_data_list:
+        if not file_data.topics:
+            continue
+
+        for _ in file_data.topics:
+            vector_matrix.append(file_data.vector_representation)
+
+    return vector_matrix
+
+
+def generate_bad_attributes_dict(prag, matrix):
+    print("Calculating Mutual info score")
+    bad_set = {}
+    row_len = matrix.shape[1]
+    for i in range(row_len):
+        if i in bad_set:
+            continue
+        col_1 = matrix[:, i]
+        for j in range(row_len):
+            print(f"iteration i: {i}, j: {j}")
+            if j <= i:
+                continue
+            if j in bad_set:
+                continue
+            col_2 = matrix[:, j]
+            col_score = score(col_1, col_2)
+            if col_score > prag:
+                bad_set[i] = True
+                bad_set[j] = True
+    return bad_set
 
 
 if __name__ == "__main__":
